72bf4c7f036a3742d6e51b8f290504a85e6aff33
Gergely Wootsch <hello@gergely-wootsch.com>
2022-10-05 21:19:09 +0200

    Bump version to 0.7.0

--- bookmarks/__init__.py
+++ bookmarks/__init__.py
@@ -72,7 +72,7 @@ The release contains all Windows dependencies. For setting up a custom developme
 .. |label3| image:: https://img.shields.io/badge/Platform-Windows-lightgrey
    :height: 18
 
-.. |label4| image:: https://img.shields.io/badge/Version-v0.6.0-green
+.. |label4| image:: https://img.shields.io/badge/Version-v0.7.0-green
    :height: 18
 
 .. |image1| image:: ./images/active_bookmark.png
@@ -97,7 +97,7 @@ __website__ = 'https://github.com/wgergely/bookmarks'
 __email__ = 'hello@gergely-wootsch.com'
 
 #: Project version
-__version__ = '0.6.0'
+__version__ = '0.7.0'
 
 #: Project version
 __version_info__ = __version__.split('.')

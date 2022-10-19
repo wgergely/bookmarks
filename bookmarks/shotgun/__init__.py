11acdcd3c73c6b2ff040706b4ca0b6423b4be3c7
Gergely Wootsch <hello@gergely-wootsch.com>
2022-10-20 01:06:30 +0200

    Update documentation

--- bookmarks/shotgun/__init__.py
+++ bookmarks/shotgun/__init__.py
@@ -1,9 +1,11 @@
 """ShotGrid integration modules, classes and utility functions.
 
 Bookmark and asset items can be linked with their ShotGrid counterparts. The linkage
-information is stored in the bookmark item database. The utility methods allow getting
-and setting some basic information in Bookmarks and ShotGrid to synchronize cut data,
-thumbnails, descriptions.
+information is stored in the bookmark item database.
+
+Check the :mod:`¬bookmarks.shotgun.shotgun` contains the generic entity model, and
+a utility class used to collect locally set ShotGrid configuration data -
+:mod:`~bookmarks.shotgun.shotgun.ShotgunProperties`.
 
 I've also included an experimental publish script, but it was not tested.
 
